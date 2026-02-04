5021a93765b5dac08cbc01bcc76c9cb3002aabb2
OctoHato <34582182+OctoHato@users.noreply.github.com>
2026-02-04 10:21:53 -0600

    Update compile_godot.bat

--- compile_godot.bat
+++ compile_godot.bat
@@ -1,7 +1,7 @@
-scons/scons.bat platform=windows target=editor module_mono_enabled=yes module_godot_tracy_enabled=true
+scons/scons.bat platform=windows target=editor module_mono_enabled=yes module_godot_tracy_enabled=true debug_symbols=yes separate_debug_symbols=yes module_godot_tracy_enabled=true profiler=tracy profiler_path="C:\Users\je0976317\Godot\Tools\tracy"
 REM # Build export templates
-scons/scons.bat platform=windows target=template_debug module_mono_enabled=yes module_godot_tracy_enabled=true
-scons/scons.bat platform=windows target=template_release module_mono_enabled=yes module_godot_tracy_enabled=true
+scons/scons.bat platform=windows target=template_debug module_mono_enabled=yes debug_symbols=yes separate_debug_symbols=yes module_godot_tracy_enabled=true profiler=tracy profiler_path="C:\Users\je0976317\Godot\Tools\tracy"
+scons/scons.bat platform=windows target=template_release module_mono_enabled=yes
 REM # Generate glue sources
 .\bin\godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
 REM # Build .NET assemblies
