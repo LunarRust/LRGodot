d65486da3b76316818f8b6d464c49f3fab09ae2c
Juniper Mitchell <34582182+JuniperMitchell@users.noreply.github.com>
2026-08-13 11:00:53 -0500

    Update compile_godot.bat

--- compile_godot.bat
+++ compile_godot.bat
@@ -3,6 +3,6 @@ REM # Build export templates
 call scons/scons.bat platform=windows target=template_debug d3d12=yes module_mono_enabled=yes debug_symbols=yes num_jobs=12 profiler=tracy profiler_path="%USERPROFILE%\godot\tracy" &&^
 call scons/scons.bat platform=windows target=template_release d3d12=yes module_mono_enabled=yes num_jobs=12 &&^
 REM # Generate glue sources
-call .\bin\godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue &&^
+call .\bin\godot.windows.editor.x86_64.lunarust_production.mono.exe --headless --generate-mono-glue modules/mono/glue &&^
 REM # Build .NET assemblies
 call python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir .\bin --push-nupkgs-local %USERPROFILE%\NuGet\lunarust_nuget\godot_nuget_store --godot-platform=windows
