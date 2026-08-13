099f82b779211525c7933e055dc4843d60adda63
Juniper Mitchell <34582182+JuniperMitchell@users.noreply.github.com>
2026-08-13 09:57:05 -0500

    Update compile_godot.bat

--- compile_godot.bat
+++ compile_godot.bat
@@ -1,8 +1,8 @@
 call scons/scons.bat platform=windows target=editor d3d12=yes module_mono_enabled=yes production=yes dev_build=no extra_suffix=lunarust_production &&^
 REM # Build export templates
-call scons/scons.bat platform=windows target=template_debug d3d12=yes module_mono_enabled=yes debug_symbols=yes num_jobs=12 profiler=tracy profiler_path="C:\Users\je0976317\godot\tracy" &&^
+call scons/scons.bat platform=windows target=template_debug d3d12=yes module_mono_enabled=yes debug_symbols=yes num_jobs=12 profiler=tracy profiler_path="%USERPROFILE%\godot\tracy" &&^
 call scons/scons.bat platform=windows target=template_release d3d12=yes module_mono_enabled=yes num_jobs=12 &&^
 REM # Generate glue sources
 call .\bin\godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue &&^
 REM # Build .NET assemblies
-call python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir .\bin --push-nupkgs-local C:\Users\je0976317\NuGet\lunarust_nuget\godot_nuget_store --godot-platform=windows
+call python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir .\bin --push-nupkgs-local %USERPROFILE%\NuGet\lunarust_nuget\godot_nuget_store --godot-platform=windows
